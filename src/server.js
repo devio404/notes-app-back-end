ee7d05511ca6a7d465fe60c80b6037814b8137d1
ini nama <rizqiii.dio@gmail.com>
2024-07-30 16:06:36 +0700

    implemented onPreresponse for Custom Error

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
 const NotesService = require("./services/InMemory/NotesServices");
 const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const notesService = new NotesService(); // buat instance dari NotesService
@@ -22,6 +23,24 @@ const init = async () => {
       service: notesService,
       validator: NotesValidator,
     }
+  })
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal 
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   })
 
   await server.start();
