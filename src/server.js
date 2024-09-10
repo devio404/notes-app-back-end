76d0dd9b5ffc200b8b5a3f79045db7f4b89a273b
ini nama <rizqiii.dio@gmail.com>
2024-09-10 08:37:19 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+
 const notes = require("./api/notes");
-const NotesService = require("./services/postgres/NotesServices");
+const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+
+//users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const notesService = new NotesService(); // buat instance dari NotesService
+  const usersService = new UsersService();
 
-  const server = Hapi.server({
+  const server = Hapi.server({  
     port: process.env.PORT,
     host: process.env.HOST,
     routes: {
@@ -19,23 +27,32 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
-    }
-  })
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
 
-    // penanganan client error secara internal 
+    // penanganan client error secara internal
     if (response instanceof ClientError) {
       const newResponse = h.response({
-        status: 'fail',
-        message: response.message
+        status: "fail",
+        message: response.message,
       });
 
       newResponse.code(response.statusCode);
@@ -43,15 +60,13 @@ const init = async () => {
     }
 
     return h.continue;
-  })
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
-
-
 
 // const init = async () => {
 //   const server = Hapi.server({
@@ -69,4 +84,3 @@ init();
 //   await server.start();
 //   console.log(`Server berjalan pada ${server.info.uri}`);
 // };
-
